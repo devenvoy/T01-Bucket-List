9e710ddecb6b8dc526af63c34ce17de7e7969b53
Devansh <iambeast0069@gmail.com>
2024-02-16 11:56:17 +0530

    google login2

--- app/src/main/java/com/example/t01_bucketlist/BaseActivity.java
+++ app/src/main/java/com/example/t01_bucketlist/BaseActivity.java
@@ -3,21 +3,17 @@ package com.example.t01_bucketlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
-import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 public class BaseActivity extends AppCompatActivity {
 
     Dialog dialog;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-    }
 
     public void showProgress(){
         dialog = new Dialog(this);
-        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
         dialog.setContentView(R.layout.progress_dialog);
         dialog.setCancelable(false);
         dialog.show();
