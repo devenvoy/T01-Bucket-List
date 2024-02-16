cd1b3527fded0ff8caf87fc36f1422368dfbb61c
Devansh <iambeast0069@gmail.com>
2024-02-16 14:23:45 +0530

    circle image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,14 +55,11 @@ dependencies {
     implementation("com.intuit.sdp:sdp-android:1.1.0")
     implementation("com.intuit.ssp:ssp-android:1.1.0")
 
-
-
-
-
-
-
     // database
     implementation("com.google.firebase:firebase-database")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
     implementation("com.firebaseui:firebase-ui-storage:8.0.2")
+
+    // Circle Image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
