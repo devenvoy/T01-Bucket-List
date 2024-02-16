bb40314b00cf0600d68bf28811d408b6e708268d
Tejas-KTC <khunttejas5285@gmail.com>
2024-02-16 14:18:49 +0530

    Tejas_bucket

--- app/src/main/java/com/example/t01_bucketlist/Add_Bucket.java
+++ app/src/main/java/com/example/t01_bucketlist/Add_Bucket.java
@@ -2,13 +2,51 @@ package com.example.t01_bucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Add_Bucket extends AppCompatActivity {
-
+    private TextView selectedDateTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bucket);
+
+        selectedDateTV = findViewById(R.id.select_time);
+        selectedDateTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Add_Bucket.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our text view.
+                                selectedDateTV.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 }
